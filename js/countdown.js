d6a3d3e29cf4ca724880e04563ab4517c3d81135
Omar Sanchez Avelar <omar_sanchez_avelar@hotmail.com>
2020-12-22 01:14:56 -0600

    se termina la version mobile

---
+++ js/countdown.js
@@ -0,0 +1,47 @@
+const getRemainTime = deadline => {
+   let now = new Date();
+   let remainTime = (new Date(deadline) - now + 1000) / 1000;
+   let remainSeconds = ('0' + Math.floor(remainTime % 60)).slice(-2);
+   let remainMinutes = ('0' + Math.floor((remainTime / 60) % 60)).slice(-2);
+   let remainHours = ('0' + Math.floor((remainTime / 3600) % 24)).slice(-2);
+   let remainDays = Math.floor(remainTime / (3600 * 24));
+
+   return {
+      remainTime,
+      remainSeconds,
+      remainMinutes,
+      remainHours,
+      remainDays
+   }
+};
+
+let el = document.getElementById('clock');
+let clock = ['spanDays', 'spanHours', 'spanMinutes', 'spanSeconds'];
+
+for (var value of clock) 
+{
+   clock[value] = document.createElement('span')
+}
+
+let days = el.appendChild(clock['spanDays'])
+let hours = el.appendChild(clock['spanHours']);
+let minutes = el.appendChild(clock['spanMinutes']);
+let seconds = el.appendChild(clock['spanSeconds']);
+
+const countdown = (deadline, finalMessage) => {
+   const timerUpdate = setInterval(() => {
+      let t = getRemainTime(deadline);
+      days.innerHTML = `${t.remainDays}`;
+      hours.innerHTML = `${t.remainHours}`;
+      minutes.innerHTML = `${t.remainMinutes}`;
+      seconds.innerHTML = `${t.remainSeconds}`;
+
+      if (t.remainTime <= 1) {
+         clearInterval(timerUpdate);
+         el.innerHTML = finalMessage;
+      }
+   }, 1000)
+
+};
+
+countdown('Sun Jan 20 2021 02:58:00 GMT-0600', 'Feliz Dia')
